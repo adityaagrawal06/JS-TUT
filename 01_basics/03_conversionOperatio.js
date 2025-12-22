6cf2718176711ec54a8e30c473b6f2345d6ce40b
adityaagrawal06 <adityaagra06@gmail.com>
2025-12-22 18:00:07 +0000

    conversion

---
+++ 01_basics/03_conversionOperatio.js
@@ -0,0 +1,24 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "ADitya"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log((valueInNumber)); //Nan(not a number) //0 //nan //1 //NaN
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "aditya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( (booleanIsLoggedIn ))// true // false //true
+
+let somenumber = 33
+let stringnuber = String(somenumber)
+console.log( (stringnuber )) //33
+console.log( typeof(stringnuber )) //string
