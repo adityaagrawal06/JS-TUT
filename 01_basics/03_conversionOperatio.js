681ce1665c2d97686db7f109ffbf18125e1315e6
adityaagrawal06 <adityaagra06@gmail.com>
2025-12-23 06:21:21 +0000

    operation

--- 01_basics/03_conversionOperatio.js
+++ 01_basics/03_conversionOperatio.js
@@ -20,5 +20,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let somenumber = 33
 let stringnuber = String(somenumber)
-console.log( (stringnuber )) //33
-console.log( typeof(stringnuber )) //string
+// console.log( (stringnuber )) //33
+// console.log( typeof(stringnuber )) //string''
+
+// ********Operations*********//
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // 2 raise to the power of 3
+// console.log(2/2)
+// console.log(2%3)
+
+// console.log(1+"2") //12
+// console.log("1"+2) //12
+// console.log("1"+"2")//12
+// console.log("1"+2+2) //122
+// console.log(1+2+"2") //32
+
+// console.log(true) //true
+// console.log(+true) //1
+// console.log(+"") //0 
+
+let num1, num2, num3
+ num1 = num2 = num3 = 2+2 //not prefered
+
+ let gamecounter =100
+ gamecounter++
+ console.log(gamecounter) //101
