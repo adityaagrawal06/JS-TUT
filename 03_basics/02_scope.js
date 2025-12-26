f6decb4925ce4e3c7adc73b7f2d35f8528df2380
adityaagrawal06 <adityaagra06@gmail.com>
2025-12-26 17:54:19 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,25 @@
+
+// {} // when comes with function or if else it is called scope
+
+// // var c=300
+// let a= 400   //global scope
+// if(true) {
+//     let a =20    //local scope      
+//     const b =20
+//     // var c = 30
+//     console.log(a); //20
+    
+// }
+// // console.log(a); // here it will show not available bcz declared in scope
+// // console.log(b); // here it will show not available bcz declared in scope
+// // console.log(c); //30 this i swhy we dont use var
+
+// console.log(a); //400
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];                  // i is a local scope
+    
+// }
+
+
+
