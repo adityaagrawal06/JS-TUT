ee3f3fcc8331174918bbbc47e074c8faf49b763c
adityaagrawal06 <adityaagra06@gmail.com>
2025-12-29 07:29:15 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,41 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month =3
+
+switch (month) {
+    case 1 :
+        console.log("january");
+        break;
+    case 1 :
+        console.log("january");
+        break;
+    case 1 :
+        console.log("january");
+        break;
+    case 1 :
+        console.log("january");
+        break;
+    case 2 :
+        console.log("February");
+        break;
+    case 3 :
+        console.log("march");
+        break;   // agar break na ho to uske aage ka sara case execute krta h , match nhi krta h excpt default
+    case 4 :
+        console.log("april");
+        break;
+    default:
+        console.log("any other month");
+        
+        break;
+}
+
+
