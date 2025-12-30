eadecced63e507f56422373606cc6cdb8db228fe
adityaagrawal06 <adityaagra06@gmail.com>
2025-12-30 06:43:19 +0000

    for in

---
+++ 05_Iterations/for_in.js
@@ -0,0 +1,43 @@
+const myObj = {
+    js : "javascript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "swift by app",
+
+}
+
+
+// for (const key in myObj) {
+//     console.log(key);               // for only keys
+// }
+// for (const key in myObj) {
+//     console.log(myObj[key]);               // for values
+// }
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);          // for comp obj      
+// }
+
+
+///FOR ARRAYS
+
+
+
+// const programming = [ "js", "cpp","rb","py","java"]
+// for (const num in programming) {
+//     console.log(programming[num]);
+
+// }
+
+
+
+//FOR MAPS
+
+// const map = new Map()   // => unique values
+// map.set('IN', 'india')    
+// map.set('USA','united states of america')
+// map.set('FR','France')
+// map.set('IN', 'india')   
+
+// for (const key in map) {
+//     console.log(key);         // CAN'T DO ITERATION IN MAP
+// }
